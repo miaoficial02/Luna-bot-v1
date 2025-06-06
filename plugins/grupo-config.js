e6c6f21fa600fbb82f60012fc5ffa479aa0b9a63
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-06-06 16:23:55 -0300

    grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -13,11 +13,11 @@ return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*
 await conn.groupSettingUpdate(m.chat, isClose)
 
 if (isClose === 'not_announcement'){
-m.reply(`${emoji} *Ya pueden escribir en este grupo.*`)
+m.reply(`${emoji} *◉ Ahora todos los miembros pueden escribir en este grupo.♡*`)
 }
 
 if (isClose === 'announcement'){
-m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
+m.reply(`${emoji2} *Solo quienrd administran el grupo pueden escribir ahora.⚠︎*`)
 }}
 handler.help = ['group open / close', 'grupo abrir / cerrar']
 handler.tags = ['grupo']
