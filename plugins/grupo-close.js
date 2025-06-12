b6237b478dba65ca195c5b9049c9c4d986c7db4b
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-06-12 12:11:49 -0300

    Update grupo-close.js

--- plugins/grupo-close.js
+++ plugins/grupo-close.js
@@ -1,1 +1,22 @@
+const handler = async (m, { conn, isBotAdmin }) => {
+  if (!isBotAdmin) return m.reply('Necesito ser admin.')
 
+  try {
+    await conn.groupSettingUpdate(m.chat, 'announcement')
+    await conn.sendMessage(m.chat, {
+      react: { text: '✅', key: m.key }
+    })
+  } catch (e) {
+    await conn.sendMessage(m.chat, {
+      react: { text: '❌', key: m.key }
+    })
+  }
+}
+
+handler.help = ['close']
+handler.tags = ['grupo']
+handler.command = /^(close|cerrar|cerrargrupo)$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+export default handler
