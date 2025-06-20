105fc4869d57e42df69d1412fe8b78d5bc58154d
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-06-20 15:13:35 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -10,29 +10,13 @@ let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
 
 ⏤͟͟͞͞♡ @𝑖𝑡𝑠.𝑚𝑖𝑎.𝑜𝑓𝑖𝑐𝑖𝑎𝑙
 > 🜸 Rol » *Creador*
-> ✧ GitHub » https://github.com/The-King-Destroy
+> ✧ GitHub » https://github.com/miaoficial02
 
 ❒ *Colaboradores:*
 
-ᰔᩚ 𝓔𝓶𝓶𝓪 𝓥𝓲𝓸𝓵𝓮𝓽𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷 
-> 🜸 Rol » *Developer*
-> ✧ GitHub » https://github.com/Elpapiema
-
-ᰔᩚ Niño Piña
-> 🜸 Rol » *Developer*
-> ✧ GitHub » https://github.com/WillZek
-
-✧ ☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆
-> 🜸 Rol » *Developer*
-> ✧ GitHub » https://github.com/Diomar-s
-
-ᰔᩚ I'm Fz' (Tesis)
-> 🜸 Rol » *Developer*
-> ✧ GitHub » https://github.com/FzTeis
-
-ᰔᩚ Legna
-> 🜸 Rol » *Mini-Dev* 
-> ✧ GitHub » https://github.com/Legna-chan
+⏤͟͟͞͞♡ @Dany
+> 🜸 Rol: » *Developer*
+> ✧ Number: » Wa.me/525626563519
 `
 await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), fkontak)
 }
